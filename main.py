c1655c80dfb98878f1b2a5811e98cf8bff62358d
Paul-G-Bim <pgbimpro@gmail.com>
2025-11-25 10:08:45 -0400

    Update user inputs

--- main.py
+++ main.py
@@ -1,43 +1,42 @@
 while True:
     user_action = input("Type add, show, edit, delete or exit: ")
-    user_action = user_action.strip().casefold()
+    user_action = user_action.strip()
 
-    match user_action:
-        case 'add':
-            todo = input("Enter a todo: ") + '\n'
-            with open("todos.txt", "r") as file:
-                todos = file.readlines()
-            todos.append(todo)
-            with open("todos.txt", "w") as file:
-                file.writelines(todos)
+    if 'add' in user_action:
+        todo = user_action[4:] + '\n'
+        with open("todos.txt", "r") as file:
+            todos = file.readlines()
+        todos.append(todo)
+        with open("todos.txt", "w") as file:
+            file.writelines(todos)
 
-        case 'show' | 'display':
-            with open("todos.txt", "r") as file:
-                todos = file.readlines()
-            [print(f"{index + 1}- {todo.strip('\n')}") for index, todo in enumerate(todos)]
+    if 'show' in user_action:
+        with open("todos.txt", "r") as file:
+            todos = file.readlines()
+        [print(f"{index + 1}- {todo.strip('\n')}") for index, todo in enumerate(todos)]
 
-        case 'edit':
-            number = int(input("Enter the number of the todo you want to edit: ")) - 1
-            with open("todos.txt", "r") as file:
-                todos = file.readlines()
-            todos[number] = input("Edit the todo: ") + '\n'
-            with open("todos.txt", "w") as file:
-                file.writelines(todos)
+    elif 'edit' in user_action:
+        number = int(user_action[5:]) - 1
+        with open("todos.txt", "r") as file:
+            todos = file.readlines()
+        todos[number] = input("Edit the todo: ") + '\n'
+        with open("todos.txt", "w") as file:
+            file.writelines(todos)
 
-        case 'delete':
-            number = int(input("Enter the number of the todo you want to delete: ")) - 1
-            with open("todos.txt", "r") as file:
-                todos = file.readlines()
-            todo_to_delete = todos[number].strip('\n')
-            todos.pop(number)
-            with open("todos.txt", "w") as file:
-                file.writelines(todos)
-            print(f"{todo_to_delete} was successfully deleted")
+    elif 'delete' in user_action:
+        number = int(user_action[7:]) - 1
+        with open("todos.txt", "r") as file:
+            todos = file.readlines()
+        todo_to_delete = todos[number].strip('\n')
+        todos.pop(number)
+        with open("todos.txt", "w") as file:
+            file.writelines(todos)
+        print(f"{todo_to_delete} was successfully deleted")
 
-        case 'exit':
-            break
+    elif 'exit' in user_action:
+        break
 
-        case _:
-            print("Incorrect input")
+    else:
+        print("Invalid command")
 
 print('Bye!')
