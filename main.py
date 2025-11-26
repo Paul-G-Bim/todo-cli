36a1711ab739a5d3d51e6c649162ae2c30a6ed0f
Paul-G-Bim <pgbimpro@gmail.com>
2025-11-26 14:48:58 -0400

    Convert some redundant code to functions

--- main.py
+++ main.py
@@ -1,28 +1,33 @@
+def get_todos():
+    with open("todos.txt", "r") as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+def store_todos(todos_local):
+    with open("todos.txt", "w") as file_local:
+        file_local.writelines(todos_local)
+
+
 while True:
     user_action = input("Type add, show, edit, delete or exit: ")
     user_action = user_action.strip()
 
     if user_action.startswith('add'):
         todo = user_action[4:] + '\n'
-        with open("todos.txt", "r") as file:
-            todos = file.readlines()
+        todos = get_todos()
         todos.append(todo)
-        with open("todos.txt", "w") as file:
-            file.writelines(todos)
+        store_todos(todos)
 
     elif user_action.startswith('show'):
-        with open("todos.txt", "r") as file:
-            todos = file.readlines()
+        todos = get_todos()
         [print(f"{index + 1}- {todo.strip('\n')}") for index, todo in enumerate(todos)]
 
     elif user_action.startswith('edit'):
         try:
             number = int(user_action[5:]) - 1
-            with open("todos.txt", "r") as file:
-                todos = file.readlines()
+            todos = get_todos()
             todos[number] = input("Edit the todo: ") + '\n'
-            with open("todos.txt", "w") as file:
-                file.writelines(todos)
+            store_todos(todos)
         except ValueError:
             print("Invalid command, only numbers are allowed")
         except IndexError:
@@ -31,12 +36,10 @@ while True:
     elif user_action.startswith('delete'):
         try:
             number = int(user_action[7:]) - 1
-            with open("todos.txt", "r") as file:
-                todos = file.readlines()
+            todos = get_todos()
             todo_to_delete = todos[number].strip('\n')
             todos.pop(number)
-            with open("todos.txt", "w") as file:
-                file.writelines(todos)
+            store_todos(todos)
             print(f"{todo_to_delete} was successfully deleted")
         except ValueError:
             print("Invalid command, only numbers are allowed")
